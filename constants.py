93c8d9bb63cb7e92403890b773553eced6211cb4
MartinAasell <martin.aasell@hotmail.com>
2022-01-19 15:29:08 +0100

    deleted return img

--- constants.py
+++ constants.py
@@ -14,6 +14,6 @@ BIRD_IMAGES = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
-RETURN_IMG = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "return_white.png")),(45,45))
+RETURN_IMG = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "return.png")),(45,45))
 # RETURN_IMG = pygame.transform.threshold(RETURN_IMG, RETURN_IMG,(0,0,0),(255,255,255))
 STAT_FONT = pygame.font.SysFont("comicsans", 50)
